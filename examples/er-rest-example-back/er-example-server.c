39890611ccb432f22adcbeaca56f17bfbe7c2c1a
saxisa <sineoliver@vip.qq.com>
2013-04-06 15:08:29 +0200

    er-rest build passed

--- examples/er-rest-example-back/er-example-server.c
+++ examples/er-rest-example-back/er-example-server.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Matthias Kovatsch and other contributors.
+ * Copyright (c) 2011, Matthias Kovatsch and other contributors.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -44,18 +44,18 @@
 
 
 /* Define which resources to include to meet memory constraints. */
-#define REST_RES_HELLO 0
+#define REST_RES_HELLO 1
 #define REST_RES_MIRROR 0 /* causes largest code size */
-#define REST_RES_CHUNKS 0
+#define REST_RES_CHUNKS 1
 #define REST_RES_SEPARATE 1
 #define REST_RES_PUSHING 1
 #define REST_RES_EVENT 1
 #define REST_RES_SUB 1
-#define REST_RES_LEDS 0
+#define REST_RES_LEDS 1
 #define REST_RES_TOGGLE 1
 #define REST_RES_LIGHT 0
 #define REST_RES_BATTERY 0
-#define REST_RES_RADIO 0
+#define REST_RES_RADIO 1
 
 
 
@@ -92,10 +92,6 @@
 #include "er-coap-03.h"
 #elif WITH_COAP == 7
 #include "er-coap-07.h"
-#elif WITH_COAP == 12
-#include "er-coap-12.h"
-#elif WITH_COAP == 13
-#include "er-coap-13.h"
 #else
 #warning "Erbium example without CoAP-specifc functionality"
 #endif /* CoAP-specific example */
@@ -167,7 +163,7 @@ mirror_handler(void* request, void* response, uint8_t *buffer, uint16_t preferre
   unsigned int content_type = REST.get_header_content_type(request);
 
   /* The other getters copy the value (or string/array pointer) to the given pointers and return 1 for success or the length of strings/arrays. */
-  uint32_t max_age_and_size = 0;
+  uint32_t max_age = 0;
   const char *str = NULL;
   uint32_t observe = 0;
   const uint8_t *bytes = NULL;
@@ -182,22 +178,14 @@ mirror_handler(void* request, void* response, uint8_t *buffer, uint16_t preferre
   int strpos = 0;
   /* snprintf() counts the terminating '\0' to the size parameter.
    * The additional byte is taken care of by allocating REST_MAX_CHUNK_SIZE+1 bytes in the REST framework.
-   * Add +1 to fill the complete buffer, as the payload does not need a terminating '\0'. */
-  if (content_type!=-1)
-  {
-    strpos += snprintf((char *)buffer, REST_MAX_CHUNK_SIZE+1, "CT %u\n", content_type);
-  }
-  
+   * Add +1 to fill the complete buffer. */
+  strpos += snprintf((char *)buffer, REST_MAX_CHUNK_SIZE+1, "CT %u\n", content_type);
+
   /* Some getters such as for ETag or Location are omitted, as these options should not appear in a request.
    * Max-Age might appear in HTTP requests or used for special purposes in CoAP. */
-  if (strpos<=REST_MAX_CHUNK_SIZE && REST.get_header_max_age(request, &max_age_and_size))
+  if (strpos<=REST_MAX_CHUNK_SIZE && REST.get_header_max_age(request, &max_age))
   {
-    strpos += snprintf((char *)buffer+strpos, REST_MAX_CHUNK_SIZE-strpos+1, "MA %lu\n", max_age_and_size);
-  }
-  /* For HTTP this is the Length option, for CoAP it is the Size option. */
-  if (strpos<=REST_MAX_CHUNK_SIZE && REST.get_header_length(request, &max_age_and_size))
-  {
-    strpos += snprintf((char *)buffer+strpos, REST_MAX_CHUNK_SIZE-strpos+1, "SZ %lu\n", max_age_and_size);
+    strpos += snprintf((char *)buffer+strpos, REST_MAX_CHUNK_SIZE-strpos+1, "MA %lu\n", max_age);
   }
 
   if (strpos<=REST_MAX_CHUNK_SIZE && (len = REST.get_header_host(request, &str)))
@@ -287,10 +275,9 @@ mirror_handler(void* request, void* response, uint8_t *buffer, uint16_t preferre
 
   /* Set dummy header options for response. Like getters, some setters are not implemented for HTTP and have no effect. */
   REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
-  REST.set_header_max_age(response, 17); /* For HTTP, browsers will not re-request the page for 17 seconds. */
+  REST.set_header_max_age(response, 10); /* For HTTP, browsers will not re-request the page for 10 seconds. CoAP action depends on the client. */
   REST.set_header_etag(response, opaque, 2);
   REST.set_header_location(response, location); /* Initial slash is omitted by framework */
-  REST.set_header_length(response, strpos); /* For HTTP, browsers will not re-request the page for 10 seconds. CoAP action depends on the client. */
 
 /* CoAP-specific example: actions not required for normal RESTful Web service. */
 #if WITH_COAP > 1
@@ -372,16 +359,8 @@ chunks_handler(void* request, void* response, uint8_t *buffer, uint16_t preferre
 /******************************************************************************/
 #if REST_RES_SEPARATE && defined (PLATFORM_HAS_BUTTON) && WITH_COAP > 3
 /* Required to manually (=not by the engine) handle the response transaction. */
-#if WITH_COAP == 7
 #include "er-coap-07-separate.h"
 #include "er-coap-07-transactions.h"
-#elif WITH_COAP == 12
-#include "er-coap-12-separate.h"
-#include "er-coap-12-transactions.h"
-#elif WITH_COAP == 13
-#include "er-coap-13-separate.h"
-#include "er-coap-13-transactions.h"
-#endif
 /*
  * CoAP-specific example for separate responses.
  * Note the call "rest_set_pre_handler(&resource_separate, coap_separate_handler);" in the main process.
@@ -440,7 +419,7 @@ separate_finalize_handler()
       coap_packet_t response[1]; /* This way the packet can be treated as pointer as usual. */
 
       /* Restore the request information for the response. */
-      coap_separate_resume(response, &separate_store->request_metadata, REST.status.OK);
+      coap_separate_resume(response, &separate_store->request_metadata, CONTENT_2_05);
 
       coap_set_payload(response, separate_store->buffer, strlen(separate_store->buffer));
 
@@ -480,11 +459,10 @@ PERIODIC_RESOURCE(pushing, METHOD_GET, "test/push", "title=\"Periodic demo\";obs
 void
 pushing_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
-  const char *msg = "It's periodic!";
-  REST.set_header_content_type(response, 0);//REST.type.TEXT_PLAIN);
+  REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
 
   /* Usually, a CoAP server would response with the resource representation matching the periodic_handler. */
-
+  const char *msg = "It's periodic!";
   REST.set_response_payload(response, msg, strlen(msg));
 
   /* A post_handler that handles subscriptions will be called for periodic resources by the REST framework. */
@@ -499,14 +477,14 @@ pushing_periodic_handler(resource_t *r)
 {
   static uint16_t obs_counter = 0;
   static char content[11];
-  uint8_t notification[1]; /* This way the packet can be treated as pointer as usual. */
+
   ++obs_counter;
 
   PRINTF("TICK %u for /%s\n", obs_counter, r->url);
 
   /* Build notification. */
-
-  coap_init_message(notification, COAP_TYPE_NON, REST.status.OK, 0 );
+  coap_packet_t notification[1]; /* This way the packet can be treated as pointer as usual. */
+  coap_init_message(notification, COAP_TYPE_NON, CONTENT_2_05, 0 );
   coap_set_payload(notification, content, snprintf(content, sizeof(content), "TICK %u", obs_counter));
 
   /* Notify the registered observers with the given message type, observe option, and payload. */
@@ -526,10 +504,9 @@ EVENT_RESOURCE(event, METHOD_GET, "sensors/button", "title=\"Event demo\";obs");
 void
 event_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
-const char *msg = "It's eventful!";
   REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
   /* Usually, a CoAP server would response with the current resource representation. */
-  
+  const char *msg = "It's eventful!";
   REST.set_response_payload(response, (uint8_t *)msg, strlen(msg));
 
   /* A post_handler that handles subscriptions/observing will be called for periodic resources by the framework. */
@@ -549,7 +526,7 @@ event_event_handler(resource_t *r)
 
   /* Build notification. */
   coap_packet_t notification[1]; /* This way the packet can be treated as pointer as usual. */
-  coap_init_message(notification, COAP_TYPE_CON, REST.status.OK, 0 );
+  coap_init_message(notification, COAP_TYPE_CON, CONTENT_2_05, 0 );
   coap_set_payload(notification, content, snprintf(content, sizeof(content), "EVENT %u", event_counter));
 
   /* Notify the registered observers with the given message type, observe option, and payload. */
@@ -568,14 +545,11 @@ RESOURCE(sub, METHOD_GET | HAS_SUB_RESOURCES, "test/path", "title=\"Sub-resource
 void
 sub_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
+  REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+
   const char *uri_path = NULL;
-  int len;
-  int base_len;
-  REST.set_header_content_type(response, 0);//REST.type.TEXT_PLAIN);
-
-  len = REST.get_url(request, &uri_path);
-  base_len = strlen(resource_sub.url);
-
+  int len = REST.get_url(request, &uri_path);
+  int base_len = strlen(resource_sub.url);
 
   if (len==base_len)
   {
@@ -583,7 +557,7 @@ sub_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_s
   }
   else
   {
-    snprintf((char *)buffer, REST_MAX_CHUNK_SIZE, ".%.*s", len-base_len, uri_path+base_len);
+    snprintf((char *)buffer, REST_MAX_CHUNK_SIZE, ".%s", uri_path+base_len);
   }
 
   REST.set_response_payload(response, buffer, strlen((char *)buffer));
@@ -645,7 +619,7 @@ leds_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_
 /******************************************************************************/
 #if REST_RES_TOGGLE
 /* A simple actuator example. Toggles the red led */
-RESOURCE(toggle, METHOD_POST, "actuators/toggle", "title=\"Red LED\";rt=\"Control\"");
+RESOURCE(toggle, METHOD_GET | METHOD_PUT | METHOD_POST, "actuators/toggle", "title=\"Red LED\";rt=\"Control\"");
 void
 toggle_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
@@ -690,7 +664,7 @@ light_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred
   }
   else
   {
-    REST.set_response_status(response, REST.status.NOT_ACCEPTABLE);
+    REST.set_response_status(response, REST.status.UNSUPPORTED_MADIA_TYPE);
     const char *msg = "Supporting content-types text/plain, application/xml, and application/json";
     REST.set_response_payload(response, msg, strlen(msg));
   }
@@ -725,7 +699,7 @@ battery_handler(void* request, void* response, uint8_t *buffer, uint16_t preferr
   }
   else
   {
-    REST.set_response_status(response, REST.status.NOT_ACCEPTABLE);
+    REST.set_response_status(response, REST.status.UNSUPPORTED_MADIA_TYPE);
     const char *msg = "Supporting content-types text/plain and application/json";
     REST.set_response_payload(response, msg, strlen(msg));
   }
@@ -783,7 +757,7 @@ radio_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred
     }
     else
     {
-      REST.set_response_status(response, REST.status.NOT_ACCEPTABLE);
+      REST.set_response_status(response, REST.status.UNSUPPORTED_MADIA_TYPE);
       const char *msg = "Supporting content-types text/plain and application/json";
       REST.set_response_payload(response, msg, strlen(msg));
     }
--- examples/er-rest-example-back/server-only.csc
+++ examples/er-rest-example-back/server-only.csc
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <simconf>
-  <project EXPORT="discard">[APPS_DIR]/mrm</project>
-  <project EXPORT="discard">[APPS_DIR]/mspsim</project>
-  <project EXPORT="discard">[APPS_DIR]/avrora</project>
-  <project EXPORT="discard">[APPS_DIR]/serial_socket</project>
-  <project EXPORT="discard">[APPS_DIR]/collect-view</project>
-  <project EXPORT="discard">[APPS_DIR]/powertracker</project>
+  <project EXPORT="discard">[CONTIKI_DIR]/tools/cooja/apps/mrm</project>
+  <project EXPORT="discard">[CONTIKI_DIR]/tools/cooja/apps/mspsim</project>
+  <project EXPORT="discard">[CONTIKI_DIR]/tools/cooja/apps/avrora</project>
+  <project EXPORT="discard">[CONTIKI_DIR]/tools/cooja/apps/serial_socket</project>
+  <project EXPORT="discard">[CONTIKI_DIR]/tools/cooja/apps/collect-view</project>
   <simulation>
     <title>REST with RPL router</title>
+    <delaytime>-2147483648</delaytime>
     <randomseed>123456</randomseed>
     <motedelay_us>1000000</motedelay_us>
     <radiomedium>
@@ -37,7 +37,7 @@
       <moteinterface>se.sics.cooja.mspmote.interfaces.SkyButton</moteinterface>
       <moteinterface>se.sics.cooja.mspmote.interfaces.SkyFlash</moteinterface>
       <moteinterface>se.sics.cooja.mspmote.interfaces.SkyCoffeeFilesystem</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.Msp802154Radio</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyByteRadio</moteinterface>
       <moteinterface>se.sics.cooja.mspmote.interfaces.MspSerial</moteinterface>
       <moteinterface>se.sics.cooja.mspmote.interfaces.SkyLED</moteinterface>
       <moteinterface>se.sics.cooja.mspmote.interfaces.MspDebugOutput</moteinterface>
@@ -60,30 +60,7 @@
       <moteinterface>se.sics.cooja.mspmote.interfaces.SkyButton</moteinterface>
       <moteinterface>se.sics.cooja.mspmote.interfaces.SkyFlash</moteinterface>
       <moteinterface>se.sics.cooja.mspmote.interfaces.SkyCoffeeFilesystem</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.Msp802154Radio</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.MspSerial</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyLED</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.MspDebugOutput</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyTemperature</moteinterface>
-    </motetype>
-    <motetype>
-      se.sics.cooja.mspmote.SkyMoteType
-      <identifier>client</identifier>
-      <description>Erbium Client</description>
-      <source EXPORT="discard">[CONTIKI_DIR]/examples/er-rest-example/er-example-client.c</source>
-      <commands EXPORT="discard">make er-example-client.sky TARGET=sky</commands>
-      <firmware EXPORT="copy">[CONTIKI_DIR]/examples/er-rest-example/er-example-client.sky</firmware>
-      <moteinterface>se.sics.cooja.interfaces.Position</moteinterface>
-      <moteinterface>se.sics.cooja.interfaces.RimeAddress</moteinterface>
-      <moteinterface>se.sics.cooja.interfaces.IPAddress</moteinterface>
-      <moteinterface>se.sics.cooja.interfaces.Mote2MoteRelations</moteinterface>
-      <moteinterface>se.sics.cooja.interfaces.MoteAttributes</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.MspClock</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.MspMoteID</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyButton</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyFlash</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyCoffeeFilesystem</moteinterface>
-      <moteinterface>se.sics.cooja.mspmote.interfaces.Msp802154Radio</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyByteRadio</moteinterface>
       <moteinterface>se.sics.cooja.mspmote.interfaces.MspSerial</moteinterface>
       <moteinterface>se.sics.cooja.mspmote.interfaces.SkyLED</moteinterface>
       <moteinterface>se.sics.cooja.mspmote.interfaces.MspDebugOutput</moteinterface>
@@ -107,8 +84,8 @@
       <breakpoints />
       <interface_config>
         se.sics.cooja.interfaces.Position
-        <x>46.57186415376375</x>
-        <y>40.35946215910942</y>
+        <x>35.100895239785295</x>
+        <y>39.70574552287428</y>
         <z>0.0</z>
       </interface_config>
       <interface_config>
@@ -117,25 +94,11 @@
       </interface_config>
       <motetype_identifier>server</motetype_identifier>
     </mote>
-    <mote>
-      <breakpoints />
-      <interface_config>
-        se.sics.cooja.interfaces.Position
-        <x>18.638049428485125</x>
-        <y>47.55034515769599</y>
-        <z>0.0</z>
-      </interface_config>
-      <interface_config>
-        se.sics.cooja.mspmote.interfaces.MspMoteID
-        <id>3</id>
-      </interface_config>
-      <motetype_identifier>client</motetype_identifier>
-    </mote>
   </simulation>
   <plugin>
     se.sics.cooja.plugins.SimControl
     <width>259</width>
-    <z>0</z>
+    <z>5</z>
     <height>179</height>
     <location_x>0</location_x>
     <location_y>0</location_y>
@@ -149,13 +112,13 @@
       <skin>se.sics.cooja.plugins.skins.AttributeVisualizerSkin</skin>
       <skin>se.sics.cooja.plugins.skins.LEDVisualizerSkin</skin>
       <skin>se.sics.cooja.plugins.skins.AddressVisualizerSkin</skin>
-      <viewport>3.61568947862321 0.0 0.0 3.61568947862321 15.610600779367 -85.92728269158351</viewport>
+      <viewport>7.9849281638410705 0.0 0.0 7.9849281638410705 -133.27812697619663 -225.04752569190535</viewport>
     </plugin_config>
     <width>300</width>
-    <z>2</z>
-    <height>178</height>
-    <location_x>261</location_x>
-    <location_y>1</location_y>
+    <z>4</z>
+    <height>175</height>
+    <location_x>263</location_x>
+    <location_y>3</location_y>
   </plugin>
   <plugin>
     se.sics.cooja.plugins.LogListener
@@ -163,11 +126,11 @@
       <filter />
       <coloring />
     </plugin_config>
-    <width>762</width>
-    <z>3</z>
-    <height>491</height>
-    <location_x>2</location_x>
-    <location_y>182</location_y>
+    <width>560</width>
+    <z>1</z>
+    <height>326</height>
+    <location_x>1</location_x>
+    <location_y>293</location_y>
   </plugin>
   <plugin>
     se.sics.cooja.plugins.RadioLogger
@@ -186,17 +149,16 @@
     SerialSocketServer
     <mote_arg>0</mote_arg>
     <width>422</width>
-    <z>4</z>
+    <z>2</z>
     <height>74</height>
-    <location_x>578</location_x>
-    <location_y>18</location_y>
+    <location_x>39</location_x>
+    <location_y>199</location_y>
   </plugin>
   <plugin>
     se.sics.cooja.plugins.TimeLine
     <plugin_config>
       <mote>0</mote>
       <mote>1</mote>
-      <mote>2</mote>
       <showRadioRXTX />
       <showRadioHW />
       <showLEDs />
@@ -205,23 +167,23 @@
       <zoomfactor>25.49079397896416</zoomfactor>
     </plugin_config>
     <width>1624</width>
-    <z>5</z>
+    <z>3</z>
     <height>252</height>
-    <location_x>6</location_x>
-    <location_y>712</location_y>
+    <location_x>4</location_x>
+    <location_y>622</location_y>
   </plugin>
   <plugin>
     se.sics.cooja.plugins.MoteInterfaceViewer
-    <mote_arg>2</mote_arg>
+    <mote_arg>1</mote_arg>
     <plugin_config>
       <interface>Serial port</interface>
       <scrollpos>0,0</scrollpos>
     </plugin_config>
-    <width>853</width>
-    <z>1</z>
-    <height>491</height>
-    <location_x>765</location_x>
-    <location_y>182</location_y>
+    <width>702</width>
+    <z>0</z>
+    <height>646</height>
+    <location_x>564</location_x>
+    <location_y>2</location_y>
   </plugin>
 </simconf>
 
